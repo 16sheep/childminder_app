5d402c251b7881ddf0767b864fcd5eeea000b1f5
github username <github email address>
2018-07-11 09:40:58 +0100

    Add children crud

--- app/controllers/children_controller.rb
+++ app/controllers/children_controller.rb
@@ -1,13 +1,32 @@
 class ChildrenController < ApplicationController
-  before_action :set_child
+  before_action :set_child, only: [:show, :edit, :destroy, :update]
 
   def show
   end
 
   def edit
+
+  end
+
+  def update
+    @child.update(child_params)
+    child.save
+    redirect_to "/users/#{child.parent_id}"  
+  end
+
+  def new
+    @child = Child.new
+  end
+
+  def create
+    child = Child.new(child_params)
+    child.user_id = session[:user_id]
+    child.save
+    redirect_to "/users/#{child.user_id}"
   end
 
   def destroy
+    @child.destroy
   end
 
   private
@@ -17,6 +36,6 @@ class ChildrenController < ApplicationController
   end
 
   def child_params
-    params.require(:child).permit(:name, :age, :parent_id)
+    params.require(:child).permit(:name, :age, :school_id)
   end
 end
